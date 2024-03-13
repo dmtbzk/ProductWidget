fea014b5a84c089ae1ae61b7c1ce50e4074b8ea7
Coding-c <bzk.demet@gmail.com>
2024-03-13 16:06:10 +0000

    created widget component

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,12 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <h1 class="font-bold">Font</h1>
+  <main class="container min-w-[1440px] mx-auto px-4">
+    <div class="pt-16">
+      <ProductWidget />
+    </div>
   </main>
 </template>
+
+<script setup lang="ts">
+import ProductWidget from '../components/widget/ProductWidget.vue';
+
+</script>
