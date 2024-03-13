928cdaaadd8baceec103b7c19fc86509e6d799cf
Coding-c <bzk.demet@gmail.com>
2024-03-13 16:55:46 +0000

    created tooltip component

--- src/components/widget/ProductWidget.vue
+++ src/components/widget/ProductWidget.vue
@@ -3,12 +3,22 @@
     <h3 class="font-bold text-3xl pb-2">Per product widgets</h3>
     <div class="pt-4 space-x-8 flex flex-row justify-between">
       <div v-for="widget in styledWidgets" :key="widget.id" class="w-1/3">
-        {{ widget }}
         <div :class="widget.styles.bgClass" class="rounded-md px-4 py-2.5 flex flex-row items-center space-x-4">
           <TheLogo :iconFillColor="widget.styles.logoFill" />
           <div>
-            <p class="text-xs font-normal" :class="widget.styles.textClass">This product {{ widget.action }}</p>
-            <p class="text-lg font-bold" :class="widget.styles.textClass">{{ widget.amount }} {{ widget.type }}</p>
+            <p class="text-xs font-normal" :class="widget.styles.textClass">
+              This product {{ widget.action }}
+            </p>
+            <p class="text-lg font-bold" :class="widget.styles.textClass">
+              {{ widget.amount }} {{ widget.type }}
+            </p>
+          </div>
+        </div>
+        <div class="space-y-4 pt-4">
+          <div class="flex items-center flex-row justify-between">
+            <span class="text-green text-sm font-normal flex flex-row items-start">
+              Linked to Public Profile  <TheTooltip />
+            </span>
           </div>
         </div>
       </div>
@@ -20,6 +30,7 @@
 import { onMounted, ref, computed } from 'vue';
 import { useStore } from 'vuex';
 import TheLogo from './elements/TheLogo.vue';
+import TheTooltip from './elements/TheTooltip.vue';
 import { getStylesForColor, type WidgetColor } from '../../types/color';
 
 interface Widget {
---
+++ src/components/widget/elements/TheTooltip.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="relative group" @mouseleave="closeTooltip">
+    <button @mouseover="openTooltip" class="pl-1">
+      <img src="@/assets/tooltip.svg" class="w-4 h-4">
+    </button>
+    <transition
+      enter-active-class="transition-opacity ease-linear duration-200"
+      enter-from-class="opacity-0"
+      enter-to-class="opacity-100"
+      leave-active-class="transition-opacity ease-linear duration-200"
+      leave-from-class="opacity-100"
+      leave-to-class="opacity-0"
+    >
+      <div
+        class="absolute top-full left-1/2 transform -translate-x-1/2 text-center w-60 p-4 bg-white rounded-lg shadow-md"
+        v-if="isTooltipOpen"
+        @mouseenter="keepTooltip">
+        <p class="text-sm text-[#212121] text-center pb-2">
+          This widget links directly to your public profile so 
+          that you can easily share your impact with your customers. 
+          Turn it off here if you do not want the badge to link to it.
+        </p>
+        <a 
+          href="/public-profile" 
+          class="text-green hover:text-green/80 text-center font-bold"
+        >
+          View Public Profile
+        </a>
+      </div>
+    </transition>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const isTooltipOpen = ref(false);
+
+const openTooltip = () => {
+  isTooltipOpen.value = true;
+};
+
+const closeTooltip = () => {
+  isTooltipOpen.value = false;
+};
+
+const keepTooltip = () => {
+  isTooltipOpen.value = true;
+};
+</script>
+
